28a75bfcf0fc39392b1d6582eb09a79c1e67896b
alvarohurtadobo <a.hurtado.bo@gmail.com>
2019-03-30 12:02:36 -0400

    Se agrego otra pequeña descripcion

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ inputName = sys.argv[2]
 img = cv2.imread(inputName)
 imgCL = np.float32(img.reshape((-1,3)))
 criteria=(cv2.TERM_CRITERIA_EPS+cv2.TERM_CRITERIA_MAX_ITER,15,1.0)
+
 print(K,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
 ret,lab,center = cv2.kmeans(imgCL,K,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
 
@@ -24,6 +25,8 @@ res=center[lab.flatten()]
 
 res2 = res.reshape((img.shape))
 res3 = cv2.resize(res2,(320,240))
+# Mostramos la imagen:
 cv2.imshow('Output',res3)
+
 cv2.waitKey()
 
