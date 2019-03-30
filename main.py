e02520483a1e450582b8452eee82879e90eac5f1
alvarohurtadobo <a.hurtado.bo@gmail.com>
2019-03-30 08:50:26 -0400

    First commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import os 
+import sys
+import cv2
+import numpy as np
+
+# k means classification
+K = int(sys.argv[1])
+inputName = sys.argv[2]
+img = cv2.imread(inputName)
+imgCL = np.float32(img.reshape((-1,3)))
+criteria=(cv2.TERM_CRITERIA_EPS+cv2.TERM_CRITERIA_MAX_ITER,15,1.0)
+print(K,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
+ret,lab,center = cv2.kmeans(imgCL,K,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
+
+print('Accuracy: ',ret)
+
+center = np.uint8(center)
+res=center[lab.flatten()]
+
+res2 = res.reshape((img.shape))
+res3 = cv2.resize(res2,(320,240))
+cv2.imshow('Output',res3)
+cv2.waitKey()
+
